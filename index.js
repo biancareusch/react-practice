178ca61c8bbc9f89e756e5ce4a2f624b36308aa5
Bianca Reusch <reusch.bianca@gmail.com>
2022-05-17 10:54:35 -0500

    imperative vs declarative

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-
+// React - declarative
 function MainContent(){
     return (
-        <h1>Hello World!</h1>
+        <h1>Hello World</h1>
     )
 }
 
@@ -10,3 +10,10 @@ ReactDOM.render(
         <MainContent />
     </div>,
     document.getElementById("root"));
+
+
+//vanilla JS - imperative
+// const h1 = document.createElement("h1");
+// h1.textContent = "This is an imperative way to program"
+// h1.className = "header";
+// document.getElementById("root").append(h1);
