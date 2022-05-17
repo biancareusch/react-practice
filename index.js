60efdb2f19e75953d492d3b37e3bc9dafb6d3f24
Bianca Reusch <reusch.bianca@gmail.com>
2022-05-17 11:13:42 -0500

    return object

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// React - declarative
+// JSX - declarative
 function MainContent(){
     return (
         <h1>Hello World</h1>
@@ -17,3 +17,20 @@ ReactDOM.render(
 // h1.textContent = "This is an imperative way to program"
 // h1.className = "header";
 // document.getElementById("root").append(h1);
+
+const element = <h1 className="header">This is JSX</h1>
+ReactDOM.render(element, document.getElementById("root"))
+
+console.log(element)
+// output object
+// {
+//      type: "h1",
+//      key: null,
+//      ref: null,
+//      props: {
+//          className: "header",
+//          children: "This is JSX"
+//      },
+//      _owner: null,
+//      _store: {}
+// }
